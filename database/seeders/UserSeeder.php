e80b8215ac0325a1375b2f2de63ab9622f06b48b
Shafwana Musyaffa <102100735+ShafwanaMusyaffa@users.noreply.github.com>
2023-03-10 00:12:35 +0700

    SEEDER

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'nama_lengkap' => 'wanaa',
+            'no_telp' => '0895636980792',
+            'email' => 'wana@gmail.com',
+            'password' => Hash::make('12345678'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'nama_lengkap' => 'nawa',
+            'no_telp' => '0895636980792',
+            'email' => 'nawa@gmail.com',
+            'password' => Hash::make('12345678'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
